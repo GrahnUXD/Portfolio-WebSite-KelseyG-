b9f2fff6aa16d7b9e38514af34374f26eb3a5b1b
GrahnUXD <grahn.uxdesign@gmail.com>
2023-07-13 14:11:13 -0700

    css and javascript updates
    added styling and code for nav menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 console.log("So glad you are here!")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+    } else {
+    x.classname = 'navtoggle';
+}
